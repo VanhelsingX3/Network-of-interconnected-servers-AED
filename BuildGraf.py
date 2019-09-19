e3eb7336544d788ef43b0788c59b5971534eada8
MrZombie1 <48537476+MrZombie1@users.noreply.github.com>
2019-09-20 07:27:25 +0800

    Add files via upload

--- BuildGraf.py
+++ BuildGraf.py
@@ -1,69 +1,85 @@
 from recursiveJson import *
-
 class Vertex:
-    def __init__(self,name):
-        self.name = name
-        self.edges = {}
-        self.valueToEdges = {}
-    
-    def addEdge(self,node,weight = 1):
-        self.edges[node] = node.valueToEdges
+	def __init__(self,name):
+		self.name = name
+		self.edges ={}
+		self.weight = None
 
-    def addCharacteristics(self,distance,bandwidth,usersOnline,traffic,meanType):
-        self.valueToEdges["Distancia"] = distance
-        self.valueToEdges["Ancho de banda"] = bandwidth
-        self.valueToEdges["Cantidad de usuarios"] = usersOnline
-        self.valueToEdges["Cantidad de trafico"] = traffic
-        self.valueToEdges["Tipo de medio"] = meanType
+	def addEdge(self,node,distance,bandwidth,usersOnline,traffic,meanType):
+		self.edges[node.name] = self.getWeight(distance,bandwidth,usersOnline,traffic,meanType)
 
 
+	#Retortnar el peso.
+	def getWeight(self,distance,bandwidth,usersOnline,traffic,meanType):
+		return 10
+
+
+				
+
+#Clase para gestion de los vertices en el grafo.
 class Graph:
-    def __init__(self):
-        self.vertices = {}
+	def __init__(self):
+		self.vertices = {}										#{A:{B:10}}
 
-    def addVertex(self,vertex):
-        self.vertices["%s" % (vertex.name)] = vertex.edges
+	def addVertex(self,vertex):
+		self.vertices["%s" % (vertex.name)] = vertex.edges
 
-
-
-#=============================  M A I N ============================
+	def printGraph(self):
+		graf = self.vertices
+		for k,v in graf.items():
+			#print("Vertice:%s" % k)
+			for aris,w in v.items():
+				print("Vertice:%s\tArista:%s - peso:%s" % (k,aris,w)) 
 """
-grafo = Graph()
-vertex1 = Vertex("Servidor A")
-vertex1.addCharacteristics(10,100,2,60,'Coaxcial')
-
-vertex2 = Vertex("Servidor B")
-vertex2.addCharacteristics(21,200,4,120,'CAT5')
-
-vertex3 = Vertex("Servidor C")
-vertex3.addCharacteristics(30,300,6,180,'Wifi')
-
-vertex4 = Vertex("Servidor D")
-vertex4.addCharacteristics(60,400,8,240,'Fibra Optica')
-
-
-vertex1.addEdge(vertex2)
-vertex1.addEdge(vertex3)
-
-vertex2.addEdge(vertex4)
-vertex2.addEdge(vertex1)
-vertex2.addEdge(vertex3)
-
-#vertex3.addEdge(vertex4)
-vertex3.addEdge(vertex1)
-vertex3.addEdge(vertex2)
-
-#vertex4.addEdge(vertex3)
-#vertex4.addEdge(vertex2)
-
-grafo.addVertex(vertex1)
-grafo.addVertex(vertex2)
-grafo.addVertex(vertex3)
-grafo.addVertex(vertex4)
-
-json = grafo.vertices
-
-#print("Hola")
-ConvertDictToText = ConvertDictToText()
-ConvertDictToText.convert(json)
+graf = Graph()
+vertexA = Vertex("A")
+vertexB = Vertex("B")
+vertexA.addEdge(vertexB,100,50,5,70,"Wifi")
+vertexB.addEdge(vertexA,100,50,5,70,"Wifi")
+graf.addVertex(vertexA)
+graf.addVertex(vertexB)
+graf.printGraph()
 """
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------
+#Clase para encontrar las rutas de un grafo.
+class BuildPaths:
+	def __init__(self):
+		self.stack = []
+		self.paths = []
+
+	#Encuentra y guarda TODAS las rutas entre un vertice origen, a un destino.
+	def findPaths(self,start,destination,dict):
+		self.stack.append([start])                                   #Agrega el vertice inicio.
+		while self.stack:                                            #Mientras la cola tenga valores.
+			tempPath = self.stack.pop()                              #Extra el ultimo indice de la lista.
+			key = tempPath[-1]                                       #Extrae el ultimo valor del elemento.
+			for i in self.subtractLists(dict[key],tempPath):         #Llama a la funcion que 'resta' los elementos de las listas dadas, devolviendo otra lista.
+				if i == destination:                                 #Stop si el valor de la 'resta' es el destino deseado.
+					self.paths.append(tempPath + [i])                #Se agrega a la variable de rutas.
+				else:
+					self.stack.append(tempPath + [i])                #En caso que no sea el valor destino, se sigue agregando rutas, volviendo al while.
+
+	#'Resta' los valores de las listas dadas.
+	#ejm: ['A','B','C'] - ['A','C'] = ['B']
+	def subtractLists(self,listaA,listaB):
+		listTemp = []
+		for i in listaA:
+			if i in listaB:
+				pass
+			else:
+				listTemp.append(i)
+		return listTemp
+	
+	def getPaths(self):
+		return self.paths
+
+#vertexA = Vertex("A")
+#vertexA.addCharacteristics("10","50","5","200","Wifi")
+#vertexA.getWeight()
--- PrincipalWindow.py
+++ PrincipalWindow.py
@@ -18,10 +18,9 @@ class PWindow(QMainWindow):
 		self.setGeometry(0,0,450,550)					
 		#QMainWindow.setWindowFlags(self, core.Qt.FramelessWindowHint)
 		self.boxsText()
-		self.buttons()
-		
+		self.buttons()	
 		self.layoutPWindow()
-
+		#self.openMapWindow()
 		css = """
 		QMainWindow{
 	  	background-color: lightblue;
@@ -33,10 +32,7 @@ class PWindow(QMainWindow):
 
 	def boxsText(self):
 		self.boxOfCharacteristics = QTextEdit(self)
-		#self.boxOfCharacteristics.setText("Hola")
-		#self.boxOfCharacteristics.setPlaceholderText("Origin Node")
-		#self.boxOfCharacteristics.resize(100,40)
-
+	
 		self.boxOfOrigin = QLineEdit(self)
 		self.boxOfOrigin.setPlaceholderText("Nodo origen")
 		self.boxOfOrigin.resize(20,10)
@@ -44,6 +40,9 @@ class PWindow(QMainWindow):
 		self.boxOfDestiny = QLineEdit(self)
 		self.boxOfDestiny.setPlaceholderText("Nodo Destino")
 		self.boxOfDestiny.resize(20,10)
+
+		self.labelTextMessage = QLabel()
+		self.labelTextMessage.setEnabled(False)
 
 	def buttons(self):
 		self.btnFileUpload = QPushButton("Cargar archivo")
@@ -56,7 +55,8 @@ class PWindow(QMainWindow):
 		
 		self.btnCreateTable = QPushButton("Crear tabla")
 		self.btnCreateTable.setToolTip("Crear tabla con la informacion dada")
-	
+		self.btnCreateTable.clicked.connect(self.enableButtonCreateTable)
+
 	def layoutPWindow(self):
 		self.hBoxCharacteristicsLayout = QHBoxLayout()
 		self.hBoxCharacteristicsLayout.addWidget(self.boxOfCharacteristics)
@@ -74,6 +74,7 @@ class PWindow(QMainWindow):
 
 		self.vFinalLayout = QVBoxLayout()
 		self.vFinalLayout.addLayout(self.hBoxCharacteristicsLayout)
+		self.vFinalLayout.addWidget(self.labelTextMessage)
 		self.vFinalLayout.addLayout(self.hBoxOriginAndDestinyLayout)
 		self.vFinalLayout.addLayout(self.hButtonsUploadAndCreateMapLayout)
 		self.vFinalLayout.addLayout(self.hButtonCreateTableLayout)
@@ -82,129 +83,29 @@ class PWindow(QMainWindow):
 		self.orderingLayout.setLayout(self.vFinalLayout)
 		self.setCentralWidget(self.orderingLayout)
 
-#-----------------FUNCIONES A LOS WIDGET DE LA VENTANA------------------
+#-----------------FUNCIONES GENERALES------------------
 	def enableCreateMap(self):
-		convertDictToText = ConvertDictToText()
 		G = nx.DiGraph()
-
-		textOfBoxOfCharacteristics = self.boxOfCharacteristics.toPlainText()
-		textOfBoxOfCharacteristics = textOfBoxOfCharacteristics.split('\n')	
+		fig = plt.figure()
 		
-		LLOfVertex = self.extractValuesToTextFormat(textOfBoxOfCharacteristics)		
-		objectGraph = self.convertLLInDict(LLOfVertex)
-
-		g = objectGraph.vertices
+		g = self.objectGraph.vertices
 		for k,v in g.items():
-			G.add_node(str(k))
-			for edge in v:
-				G.add_node(str(edge.name))
-				G.add_edge(str(k), str(edge.name), weight=1)
-		#convertDictToText.convert(objectGraph.vertices,"PruebaJsonWithDict.txt")
+			#G.add_node(str(k))
+			for aris,w in v.items():
+				G.add_node(str(k))
+				G.add_edge(str(k), str(aris),weight=int(w),background_weight='black')
+
 		pos = nx.spring_layout(G)
-		nx.draw(G,rows=True, with_labels=True,node_color='r',node_size=4000,edge_color='b',arrowsize=30)
-		#plt.savefig("plot.png")
-		plt.show()
-
-
-
-		"""
-#===========================GRAFICANDO SOLO CON LA LISTA ENLAZADA==============================================
-
-		for i in range(LLOfVertex.length()):
-			item = LLOfVertex.atPosition(i)
-			G.add_node(str(item.name))
-			for j in range(item.edges.length()):
-				itemVertex = item.edges.atPosition(j)
-				G.add_edge(str(item.name),str(itemVertex.name), weight=5)
+		nx.draw(G,pos,rows=True, with_labels=True,node_color='skyblue',node_shape="o",node_size=4000,edge_color='lightblue',arrowsize=20,font_color='white',style='dashed')		
+		labels = nx.get_edge_attributes(G,'weight')
+		nx.draw_networkx_edge_labels(G,pos,edge_labels=labels,font_family='sans-serif')
 		
-		edg = [('Servidor A', 'Servidor C')]
-		pos = nx.spring_layout(G)
-		nx.draw(G,rows=True, with_labels=True,node_color='r',node_size=4000,edge_color='b',arrowsize=30)
+		#fig.set_facecolor("#00000F")
+		plt.savefig('graph.png') #facecolor=fig.get_facecolor() )
 		
-		self.imageGraph = plt.savefig("plot.png")
-		mapWindow = MapWindow()
-		plt.show()
-		"""		
-
-	#Extrae todos los vertices,aristas y caracteristicas del contenido de la caja de texto guardandolos en una lista enlazada.
-	def extractValuesToTextFormat(self,textOfBoxOfCharacteristics):	
-		vertexLL = LinkedList()
-		for i in range(len(textOfBoxOfCharacteristics)):						#Recorrera el contenido de la caja, para obtener los vetices.
-			currentItem = textOfBoxOfCharacteristics[i]
-			if(currentItem.find('\t') == -1):
-				findItem = ("\t%s" % currentItem)
-				if(findItem != '\t'):
-					for j in range(len(textOfBoxOfCharacteristics)):
-						currentItemSecundaryItem = textOfBoxOfCharacteristics[j]
-						item = currentItemSecundaryItem.strip()
-						if(currentItemSecundaryItem.find(findItem) != -1):
-							name = item
-							bandwidth = self.extractValueToString(textOfBoxOfCharacteristics[j+1])
-							usersOnline = self.extractValueToString(textOfBoxOfCharacteristics[j+2])
-							traffic	= self.extractValueToString(textOfBoxOfCharacteristics[j+3])
-							distance = self.extractValueToString(textOfBoxOfCharacteristics[j+4])
-							meanType = self.extractValueToString(textOfBoxOfCharacteristics[j+5])
-							vertexLL.add(name,distance,bandwidth,usersOnline,traffic,meanType)
-							#print("%s,%s,%s,%s,%s,%s" % (name,bandwidth,usersOnline,traffic,distance,meanType))
-							break
+		self.openMapWindow()
 		
-		for k in range(len(textOfBoxOfCharacteristics)):						#Recorrera el contenido de la caja, para obtener las aristas.
-			itemVertex = textOfBoxOfCharacteristics[k]
-			if(itemVertex.find('\t') == -1):
-				for m in range(len(textOfBoxOfCharacteristics)):
-					if(textOfBoxOfCharacteristics[m] == itemVertex):
-						#print("------------%s --------------" % itemVertex)
-						index = m + 1
-						if(index < len(textOfBoxOfCharacteristics)):
-							while(textOfBoxOfCharacteristics[index].count('\t') >= 1):
-								if(textOfBoxOfCharacteristics[index].count('\t') == 1):
-									itemT = textOfBoxOfCharacteristics[index].strip()
-									name = itemT
-									bandwidth = self.extractValueToString(textOfBoxOfCharacteristics[index+1])
-									usersOnline = self.extractValueToString(textOfBoxOfCharacteristics[index+2])
-									traffic	= self.extractValueToString(textOfBoxOfCharacteristics[index+3])
-									distance = self.extractValueToString(textOfBoxOfCharacteristics[index+4])
-									meanType = self.extractValueToString(textOfBoxOfCharacteristics[index+5])									
-									#print("%s,%s,%s,%s,%s,%s" % (name,bandwidth,usersOnline,traffic,distance,meanType))
-									#print(name)
-									vertexLL.searchInLL(itemVertex).edges.add(itemT,textOfBoxOfCharacteristics[m+1],None,None,None,None)
-								index += 1
-		return vertexLL
-	
-	#Convierte los datos extraidos del texto, a un diccionario, para luego crear el mapa.
-	def convertLLInDict(self,LLOfVertex):
-		graph = Graph()
-		LLOfEdges = LinkedList()
-		for i in range(LLOfVertex.length()):
-			currentNode = LLOfVertex.atPosition(i)
-			vertex = Vertex(currentNode.name)
-			LLOfEdges.add(currentNode.name,vertex,None,None,None,None)					#Almacena los vertices 
-			vertex.addCharacteristics(currentNode.distance,currentNode.bandwidth,currentNode.usersOnline,currentNode.traffic,currentNode.meanType)
-			
-		
-		for j in range(LLOfEdges.length()):
-			currentItem = LLOfEdges.atPosition(j)
-			item = LLOfVertex.searchInLL(currentItem.name)								#Arista a agregar
-			for k in range(item.edges.length()):
-				currentEdge = item.edges.atPosition(k).name
-
-				vertexToAdd = LLOfEdges.searchInLL(currentEdge).distance			#Aqui esta guardado la informacion de tipo VERTEX
-				currentItem.distance.addEdge(vertexToAdd)
-			graph.addVertex(currentItem.distance)
-		return graph
-
-
-
-
-
-	#Extrae el valor de una caracteristica(str). ej: Distancia:100 => 100
-	def extractValueToString(self,string,toFind = ":"):
-		indexFindChar = string.find(toFind)
-		_len = len(string)
-		newChar = string[indexFindChar+1 : _len]
-		return newChar	
-				
-		
+	#Carga el archivo de texto, y lo excribe en la caja de EditText
 	def enableButtonUpload(self):
 		filePath = QFileDialog.getOpenFileName(self, "Selecciona el archivo a cargar.")
 		filePath = filePath[0]
@@ -216,27 +117,199 @@ class PWindow(QMainWindow):
 			tempList.append(i)
 		f.close()
 		self.content = "".join(tempList)
-		self.boxOfCharacteristics.setText(self.content)
+		self.boxOfCharacteristics.setText(self.content)								#Traspasa el contenido cargado a la caja de texto.
+
+		LLOfVertex = self.extractValuesToTextFormat()		
+		self.objectGraph = self.convertLLInDict(LLOfVertex)
+
+
+	def enableButtonCreateTable(self):
+		newJson = {}
+		buildPaths = BuildPaths()
+		jsonGraph = self.objectGraph.vertices
+		paths = None
+		for k,v in jsonGraph.items():
+			newJson[k]=[]
+			for z in v:
+				print(type(z.valueToEdges))
+				print(z.valueToEdges)
+				newJson[k].append(z.name)
+
+		origin = self.boxOfOrigin.text()
+		destination = self.boxOfDestiny.text()
+		stateO,stateD = self.checkVertexExists(origin,destination,newJson)
+
+		#------------Control del input del origen y destino------------
+		if(stateO == True and stateD == True):
+			buildPaths.findPaths(origin,destination,newJson)
+			paths = buildPaths.getPaths()
+			self.labelTextMessage.setVisible(False)
 		
+		elif(stateO == True and stateD == False):
+			self.enableLabel(True,'El valor destino no existe')
+			print("True","False")
+		
+		elif(stateO == False and stateD == True):
+			print("False,True")
+			self.enableLabel(True,'El valor origen no existe')
+		
+		elif(stateO == False and stateD == False):
+			self.enableLabel(True,'El valor origen y destino no existen')
+			print("False,False")				
+
+		#------------Generando el contenido a mostrar en la caja de texto------------
+		title = ("%s\n%s%s%s\n%s\n" % ('-'*110,'\t'*2,'T A B L A  D E  R U T A S','\t'*2,'-'*110))
+		titleSubTable = ("%s" % ('\tPeso\t|\tRuta\n'))
+		dates = ""
+		for i in paths:
+			temp = ("'\tPeso\t|\t%s\n" % (i))
+			dates = dates + temp
+
+		content = title + titleSubTable + dates
+		self.openTableWindows(content)
+
+	#-----------------------------FUNCIONES LLAMADAS POR LAS GENERALES----------------------------------------------
+	#Devuelve boolean dependiendo de si el valor origen y destino existe en el diccionario.
+	def checkVertexExists(self,origin,destination,json):
+		tempArr = []
+		for k in json.keys():
+			tempArr.append(k)
+
+		for i in tempArr:
+			if(origin in tempArr and destination not in tempArr):						
+				return True,False
+
+			if(origin not in tempArr and destination in tempArr):
+				return False,True
+			
+			if(origin not in tempArr and destination not in tempArr):
+				return False,False
+
+			if(origin in tempArr and destination in tempArr):
+				return True,True
+
+	#Extrae todos los vertices,aristas y caracteristicas del contenido de la caja de texto guardandolos en una lista enlazada.
+	def extractValuesToTextFormat(self):	
+		textOfBoxOfCharacteristics = self.boxOfCharacteristics.toPlainText()
+		textOfBoxOfCharacteristics = textOfBoxOfCharacteristics.split('\n')
+		vertexLL = LinkedList()
+		for i in range(len(textOfBoxOfCharacteristics)):										#Recorrera el contenido de la caja, para obtener los vetices.
+			currentLine = textOfBoxOfCharacteristics[i]											#Linea actual
+			if(currentLine.find('\t') == -1):													#El vertice(no tiene \t)
+				#print(currentLine)
+				findItem = ("\t%s" % currentLine)
+				for j in range(len(textOfBoxOfCharacteristics)):
+					currentItemSecundaryItem = textOfBoxOfCharacteristics[j]
+					if(currentItemSecundaryItem == findItem):
+						name = currentItemSecundaryItem.strip()											#Arista con el mismo nombre del vertice actual, para extraer sus caracteristicas.
+						vertexLL.add(name,None,None,None,None,None)
+						#print("%s,%s,%s,%s,%s,%s" % (name,bandwidth,usersOnline,traffic,distance,meanType))
+						break
+		#vertexLL._printToNormal()
+		
+		for k in range(len(textOfBoxOfCharacteristics)):						#Recorrera el contenido de la caja, para obtener las aristas.
+			itemVertex = textOfBoxOfCharacteristics[k]
+			if(itemVertex.find('\t') == -1):
+				#print(itemVertex)
+				if(vertexLL.searchInLL(itemVertex,1) == True):
+					for m in range(len(textOfBoxOfCharacteristics)):
+						currentEdge = textOfBoxOfCharacteristics[m]	
+						if(currentEdge == itemVertex):
+							#print(currentEdge)
+							index = m + 1
+							if(index < len(textOfBoxOfCharacteristics)):
+								while(textOfBoxOfCharacteristics[index].count('\t') >= 1):
+									if(textOfBoxOfCharacteristics[index].count('\t') == 1):
+										name = textOfBoxOfCharacteristics[index].strip()
+										distance = self.extractValueToString(textOfBoxOfCharacteristics[index+1]).strip()
+										meanType = self.extractValueToString(textOfBoxOfCharacteristics[index+2]).strip()
+										bandwidth = self.extractValueToString(textOfBoxOfCharacteristics[index+3]).strip()
+										usersOnline = self.extractValueToString(textOfBoxOfCharacteristics[index+4])
+										traffic = self.extractValueToString(textOfBoxOfCharacteristics[index+5])
+										vertexLL.searchInLL(itemVertex).edges.add(name,bandwidth,usersOnline,traffic,distance,meanType)
+									index += 1
+		#vertexLL.first.next.edges._printToNormal()
+		return vertexLL
+
+	#Convierte los datos extraidos del texto, a un diccionario, para luego crear el mapa.
+	def convertLLInDict(self,LLOfVertex):
+		graph = Graph()
+		LLOfEdges = LinkedList()
+		for i in range(LLOfVertex.length()):											#Recorre los vertices de la lista enlazada.
+			currentVertex = LLOfVertex.atPosition(i)									#Vertice actual.
+			vertex = Vertex(currentVertex.name)
+			LLOfEdges.add(currentVertex.name,vertex,None,None,None,None)					#Almacena los vertices 
+			#vertex.addCharacteristics(currentVertex.distance,currentVertex.bandwidth,currentVertex.usersOnline,currentVertex.traffic,currentVertex.meanType)
+			
+		for j in range(LLOfVertex.length()):
+			currentVertex = LLOfVertex.atPosition(j)
+			vertex = LLOfEdges.searchInLL(currentVertex.name).distance
+			for k in range(currentVertex.edges.length()):
+				edge = currentVertex.edges.atPosition(k)					#Nodo arista
+				objEdge = LLOfEdges.searchInLL(edge.name)					#Nodo arista(class vertex)
+				vertex.addEdge(objEdge,edge.distance,edge.bandwidth,edge.usersOnline,edge.traffic,edge.meanType)
+			
+			graph.addVertex(vertex)
+
+		return graph
+
+	#Extrae el valor de una caracteristica(str). ej: Distancia:100 => 100
+	def extractValueToString(self,string,toFind = ":"):
+		indexFindChar = string.find(toFind)
+		_len = len(string)
+		newChar = string[indexFindChar+1 : _len]
+		return newChar
+
+	def enableLabel(self,state,text = ''):
+		self.labelTextMessage.setVisible(state)
+		self.labelTextMessage.setEnabled(state)
+		self.labelTextMessage.setText(text)
+		self.labelTextMessage.setStyleSheet('QLabel {color: red}')
+		self.labelTextMessage.setFont(QFont("Times Font", 10))	
+				
 	def centerWindow(self):
 		screen = QDesktopWidget().screenGeometry()
 		size = self.geometry()
 		self.move(size.width()*1.5,size.height())
 
+	def openMapWindow(self):
+		self.a = MapWindow()
+		self.a.show()
 
+	def openTableWindows(self,content):
+		self.b = TableWindow(content)
+		self.b.show()
 
-
+#Ventana que se crea para cargar la imagen del grafo.
 class MapWindow(QDialog):
 	def __init__(self, parent = None):
 		QDialog.__init__(self, parent)
+		self.setWindowTitle("Grafica del grafo")
+		
 		label = QLabel(self)
-		#pixmap = QPixmap('plot.png')
-		#pixmap.scaled(20, 20)
-		#label.setPixmap(pixmap)
-		#self.resize(pixmap.width(),pixmap.height())
+		pixmap = QPixmap('graph.png')
+		pixmap.scaled(20, 20)
+		label.setPixmap(pixmap)
+		self.resize(pixmap.width(),pixmap.height())
 
+class TableWindow(QWidget):
+	def __init__(self,contentPaths):
+		QWidget.__init__(self)
+		self.contentPaths = contentPaths
+		self.setWindowTitle("Tabla de rutas")
+		self.setGeometry(0,0,450,450)
+		self.center()
+		content = QTextEdit(self)
+		content.setGeometry(0,0,450,450)
+		content.setText(self.contentPaths)
+		
+	def center(self):
+		screen = QDesktopWidget().screenGeometry()
+		size = self.geometry()
+		self.move(size.width()/2,size.height()/2)
 
-
+		
+	
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	#app.setWindowIcon(QtGui.QIcon("images/icon_application.png"))
